899facca74f93e402916d496698e7ed49b5d7223
zoomdong <1344492820@qq.com>
2020-04-24 00:53:11 +0800

    feat: 调整目录

---
+++ day4/for-in.py
@@ -0,0 +1,14 @@
+sum = 0
+for i in range(1, 101):
+  sum += i
+print(sum)
+
+sum1 = 0
+for i in range(1, 101, 2):
+  sum1 += i
+print(sum1)
+
+sum2 = 0
+for i in range(2, 101, 2):
+  sum2 += i
+print(sum2)
